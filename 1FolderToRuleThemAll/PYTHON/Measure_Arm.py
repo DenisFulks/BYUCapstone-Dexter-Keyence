132174c486303c5227357741c02ca0eee8e4904e
DenisFulks <denisfulks@gmail.com>
2025-08-19 13:16:42 -0400

    Test Scan

--- 1FolderToRuleThemAll/PYTHON/Measure_Arm.py
+++ 1FolderToRuleThemAll/PYTHON/Measure_Arm.py
@@ -21,14 +21,14 @@ def main(filename=None, auto_flag=False, scan_type='live', ui=None, debug_flag=F
         scan1.show_cloud()
 
     # Find bar faces
-    scan1.fit_bar_faces(plotNum=0, cutOff=[-65, 500], show=debug_flag)    #-25, 300
+    scan1.fit_bar_faces(plotNum=0, cutOff=[0, 200], show=debug_flag)    #-25, 300
     # print(f'Bar Axis: {scan1.bar_axis}')
 
     # Find spindle
     #scan1.fit_spindle(axial_cutoff=-100, num_bins=100, circle_fit_tol=0.18, show=True, plot=False)
     #scan1.fit_spindle2(axial_cutoff=-80, num_bins=80, circle_fit_tol=0.2, circle_resid_tol=[1.0], min_fit_points=300, centers_resid_tol=[2.0], show=False, plot=False)
     # print(f'Spindle Axis: {scan1.axis_dir}')
-    scan1.fit_spindle_3D3(axial_cutoff=-150, side='left', show_flag=debug_flag, plot_flag=False, box_size=8.0)     #-70
+    scan1.fit_spindle_3D3(axial_cutoff=-140, side='left', show_flag=debug_flag, plot_flag=False, box_size=8.0)     #-70
     # print(f'Spindle Axis: {scan1.spindle_axis}')
 
     # scan1.visualize_axes(length=100)
